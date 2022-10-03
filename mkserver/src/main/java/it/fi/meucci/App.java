117d1beefaf38b4426f872538226a3a57769c195
lai2-01 <lai2-01@itismeucci.com>
2022-10-03 10:17:25 +0200

    primo commit

---
+++ mkserver/src/main/java/it/fi/meucci/App.java
@@ -0,0 +1,17 @@
+package it.fi.meucci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ServerStr server = new ServerStr();
+            server.attendi();
+            server.comunica();
+    }
+
+}
+
